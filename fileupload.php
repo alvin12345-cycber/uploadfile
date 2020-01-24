3b11c18980cc1722efb0c965dcfe2afcbecb1152
alvin12345-cycber <57780639+alvin12345-cycber@users.noreply.github.com>
2020-01-24 11:44:37 +0800

    Add files via upload

--- fileupload.php
+++ fileupload.php
@@ -4,11 +4,10 @@
 <title>File Uploading</title>
   </head>
  <body>
-<form method= "post" action= "uploadtoserver.php" enctype="multipart/form-data"
+<form method= "post" action= "uploadtoserver.php" enctype="multipart/form-data">
 <label for = "covername">Upload image:</label>
 <input type="file" name="file"/>
 <input type="submit" value="Upload" name="submit">
-
        </form>
    </body>
 </html>
--- uploadtoserver.php
+++ uploadtoserver.php
@@ -1,21 +1,27 @@
 <?php
-$targetDir= "uploads/";
-$fileName=basename($_FILES['file']['name']);
-$targetFilePath= $targetDir.$fileName;
-$fileType= pathinfo($targetFilePath,PATHINFO_EXTENSION);
-
-if(isset($_POST["submit"]) && !empty($_FILES['file']['name'])){
-	$allowTypes = array('jpg','png','gif','pdf');
-	if(in_array($fileType, $allowTypes)){
-		if(move_uploaded_file($_FILES['file']['tmp_name'], $targetFilePath)){
-			echo $fileName;
-	}    else{
-		    echo "Failed to upload";
-		}
-}
-	 else{
-	 	echo "File type not allowed!";
-	 }
-} 
-
-?>	
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size = $_FILES['image']['size'];
+      $file_tmp = $_FILES['image']['tmp_name'];
+      $file_type = $_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
